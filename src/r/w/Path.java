92fad1554b0e77552bf26d4ef15c08717eec7239
hk1220 <66093512+hk1220@users.noreply.github.com>
2020-07-29 20:13:25 +0200

    Update Path.java

--- src/r/w/Path.java
+++ src/r/w/Path.java
@@ -5,31 +5,26 @@ import java.nio.file.Paths;
 public class Path {
 
     private static String path;
-    //Erzeugt den Pfad zur CSV DATEI
+    
     public static String getPathofMedikamentenliste(){
         path = Paths.get("").toAbsolutePath().toString()+ "\\medicines_output_european_public_assessment_reports.csv" ;
-        System.out.println(path + " aus Klasse Path");
         return (path);
     }
 
     public static String getPathofDarreichungsformen(){
         path = Paths.get("").toAbsolutePath().toString()+ "\\Darreichungsformen.csv" ;
-        System.out.println(path + " aus Klasse Path");
         return (path);
     }
     public static String getPathofMedikamentenakte(){
         path = Paths.get("").toAbsolutePath().toString()+ "\\Medikamentenakte.csv" ;
-        System.out.println(path + " aus Klasse Path");
         return (path);
     }
     public static String getPathofTerminbuchungen(){
         path = Paths.get("").toAbsolutePath().toString()+ "\\Terminbuchungen.csv" ;
-        System.out.println(path + " aus Klasse Path");
         return (path);
     }
     public static String getPathofPatientenakte(){
         path = Paths.get("").toAbsolutePath().toString()+ "\\Patientenakte.csv" ;
-        System.out.println(path + " aus Klasse Path");
         return (path);
     }
 }
